c029acc9595a5b4017f30ab48d8d5a6451001e48
German O. Macchia <germanmacchia@gmail.com>
2026-08-15 15:59:11 -0300

    feat: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { authGuard } from './core/guards/auth.guard';
+import { LoginComponent } from './features/auth/login/login.component';
+import { HeroListComponent } from './features/heroes/hero-list/hero-list.component';
+
+export const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'heroes', component: HeroListComponent, canActivate: [authGuard] },
+    { path: '', pathMatch: 'full', redirectTo: 'heroes' },
+    { path: '**', redirectTo: 'heroes' },
+];
