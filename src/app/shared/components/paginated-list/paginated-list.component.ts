759a28ff323b5833211869bdf8712f509fdd9639
German O. Macchia <germanmacchia@gmail.com>
2026-08-16 14:38:36 -0300

    feat:paginated-list

--- src/app/shared/components/paginated-list/paginated-list.component.ts
+++ src/app/shared/components/paginated-list/paginated-list.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, input, output } from '@angular/core';
+import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
 
 @Component({
     selector: 'app-paginated-list',
-    imports: [],
+    imports: [MatPaginatorModule],
     templateUrl: './paginated-list.component.html',
     styleUrl: './paginated-list.component.scss',
 })
-export class PaginatedListComponent {}
+export class PaginatedListComponent {
+    readonly total = input.required<number>();
+    readonly pageIndex = input(0);
+    readonly pageSize = input(8);
+    readonly pageSizeOptions = input([4, 8, 12, 16]);
+
+    readonly pageChange = output<PageEvent>();
+
+    onPage(event: PageEvent): void {
+        this.pageChange.emit(event);
+    }
+}
