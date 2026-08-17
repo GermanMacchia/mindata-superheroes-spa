b4e4c76901372066c0233d69236e87d005b685b8
German O. Macchia <germanmacchia@gmail.com>
2026-08-17 15:26:53 -0300

    chore: uppercase en card

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -5,10 +5,11 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 
 import { SuperHero } from '@app/features/heroes/models/super-hero.model';
+import { UppercaseDirective } from '@app/shared/directives/uppercase.directive';
 
 @Component({
     selector: 'app-card',
-    imports: [MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule],
+    imports: [MatCardModule, MatButtonModule, MatIconModule, MatTooltipModule, UppercaseDirective],
     templateUrl: './card.component.html',
     styleUrl: './card.component.scss',
 })
