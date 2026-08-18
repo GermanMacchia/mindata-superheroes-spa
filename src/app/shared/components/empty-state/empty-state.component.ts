6b6e97794a66188dfe5e227d6983fa61730f9545
GermanMacchia <germanmacchia@gmai.com>
2026-08-18 13:36:44 -0300

    feat: empty state

--- src/app/shared/components/empty-state/empty-state.component.ts
+++ src/app/shared/components/empty-state/empty-state.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
     selector: 'app-empty-state',
-    imports: [],
+    imports: [MatIconModule],
     templateUrl: './empty-state.component.html',
     styleUrl: './empty-state.component.scss',
 })
-export class EmptyStateComponent {}
+export class EmptyStateComponent {
+    readonly icon = input('search_off');
+    readonly message = input.required<string>();
+}
