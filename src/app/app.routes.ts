3b3b9ef0cfdb198c75bfd1cda8eb9421b19a0f27
GermanMacchia <Germanmacchia@gmail.com>
2026-09-03 12:29:26 -0300

    perf: rutas con lazyloading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,29 @@
 import { Routes } from '@angular/router';
 
 import { appGuard, authGuard } from './core/guards/auth.guard';
-import { LoginComponent } from './features/auth/login/login.component';
-import { HeroDetailComponent } from './features/heroes/hero-detail/hero-detail.component';
 import { heroResolver } from './features/heroes/hero-detail/hero.resolver';
-import { HeroListComponent } from './features/heroes/hero-list/hero-list.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent, canActivate: [appGuard] },
-    { path: 'heroes', component: HeroListComponent, canActivate: [authGuard] },
+    {
+        path: 'login',
+        loadComponent: () =>
+            import('./features/auth/login/login.component').then((m) => m.LoginComponent),
+        canActivate: [appGuard],
+    },
+    {
+        path: 'heroes',
+        loadComponent: () =>
+            import('./features/heroes/hero-list/hero-list.component').then(
+                (m) => m.HeroListComponent,
+            ),
+        canActivate: [authGuard],
+    },
     {
         path: 'heroe/:id',
-        component: HeroDetailComponent,
+        loadComponent: () =>
+            import('./features/heroes/hero-detail/hero-detail.component').then(
+                (m) => m.HeroDetailComponent,
+            ),
         canActivate: [authGuard],
         resolve: { hero: heroResolver },
     },
